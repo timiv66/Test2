7f8cd02d482c3f0139e11ba4ddd5f8ba1e9278e3
Timi Verissimo <timiv9@gmail.com>
2024-11-17 15:53:35 -0500

    Added test case for paying for order

--- src/test2/CustomerOrderTest.java
+++ src/test2/CustomerOrderTest.java
@@ -23,10 +23,23 @@ class CustomerOrderTest {
     
     // ToDo: Test placing a valid order
  	// Add items to the order, verify the number of items, and check if the total cost is calculated correctly.
-
+    @Test
+	public void testPlaceOrder() {
+	    order.addItem("Pepperoni Pizza", 12.50);
+	    order.addItem("Garlic Bread", 5.00);
+	    assertEquals(2, order.getItems().size());
+	    assertEquals(17.50, order.getTotalAmount(), 0.01);
+	}
 
  	// ToDo: Test paying for the order
  	// Add an item to the order, pay using a method, and verify that the order is marked as paid.
+    @Test
+   	public void testPayOrder() {
+   	    order.addItem("Cheese Pizza", 10.00);
+   	    order.pay("Credit Card");
+   	    assertTrue(order.isPaid());
+   	    assertEquals(10.00, order.getTotalAmount(), 0.01);
+   	}
 
 
  	// ToDo: Test inventory update when an order is placed
